89430fea55eb34056035992e38dd3236f064e054
Carolyn Harris <ccharris27@gmail.com>
2018-04-16 15:42:32 -0400

    things

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,10 @@ export default class Footer extends Component {
                         <a className="asocial" href="https://www.instagram.com/letswaitdatingapp/"><div className="social"><i className="fab fa-instagram"></i></div></a>
                         <a className="asocial" href="https://www.facebook.com/LetsWait-276298222909777/"><div className="social"><i className="fab fa-facebook-f"></i></div></a>
                         <a className="asocial" href="https://twitter.com/letswaitdating"><div className="social"><i className="fab fa-twitter"></i></div></a>
-                        <p style={{width: '450px', paddingTop: '12px', marginLeft: '0', color: '#fff'}}>&copy; LetsWait 2018, All Rights Reserved.</p>
+                </div>
+                <div>
+                    <p style={{width: '450px', paddingTop: '12px', marginLeft: '-20px', color: '#fff'}}><a style={{color: '#fff'}} href="#">Contact</a> / <a style={{color: '#fff'}} href="#">Privacy</a> / <a style={{color: '#fff'}} href="#">Press</a> <br /></p>
+                    <p style={{color: '#fff', width: '450px', marginLeft: '-40px', marginBottom: '0'}}>&copy; LetsWait 2018, All Rights Reserved.</p>
                 </div>
             </div>
         )
